e5fec055cc22434f2ea088543939a2c7e23c7a88
arkeks <theincrediblelynx@yandex.ru>
2021-11-04 21:29:03 +0300

    Made quicksort.

---
+++ code/quick_sort.h
@@ -0,0 +1,12 @@
+#ifndef G_HW_1_QUICK_SORT_H
+#define G_HW_1_QUICK_SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stddef.h>
+
+uint64_t* arr_sum (uint64_t* less_arr, size_t less_size, uint64_t* gr_arr, size_t gr_size);
+uint64_t* quicksort (uint64_t* array, size_t array_length);
+
+#endif // G_HW_1_QUICK_SORT_H
