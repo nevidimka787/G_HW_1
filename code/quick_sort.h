4a1a4bad78de690747e5cc82abca66e90b120159
arkeks <theincrediblelynx@yandex.ru>
2021-11-04 21:43:52 +0300

    Fixed quicksort again.

--- code/quick_sort.h
+++ code/quick_sort.h
@@ -8,5 +8,6 @@
 
 uint64_t* arr_sum (uint64_t* less_arr, size_t less_size, uint64_t* gr_arr, size_t gr_size);
 uint64_t* quicksort (uint64_t* array, size_t array_length);
+void q_sort (uint64_t* array, size_t array_length);
 
 #endif // G_HW_1_QUICK_SORT_H
